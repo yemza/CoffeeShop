6a40f1387c07bf160ae72a7cf4329ae4efa1a98d
hamza.elbouazzaoui <hamza.elbouazzaoui@sully-group.com>
2023-06-11 22:06:57 +0100

    Adding City and Country controlers

--- WebModule/src/main/java/com/TheCoffeShop/Rest/CountryControler.java
+++ WebModule/src/main/java/com/TheCoffeShop/Rest/CountryControler.java
@@ -1,4 +1,50 @@
 package com.TheCoffeShop.Rest;
 
+
+import com.TheCoffeShop.DAOEntities.Country;
+import com.TheCoffeeShop.Services.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CountryControler {
-}
+
+      private CountryService countryService ;
+
+
+  CountryControler(CountryService countryService){
+        this.countryService = countryService;
+      }
+
+
+  @GetMapping("countries")
+  public List<Country> getCountries(){
+    return null;
+  }
+
+  @GetMapping("country")
+  public Country getCountry(@PathVariable Long id) {
+    return null ;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
