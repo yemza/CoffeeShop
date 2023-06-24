1110a585b4cb859a9071243fb273c0aab86a509c
hamza.elbouazzaoui <hamza.elbouazzaoui@sully-group.com>
2023-06-24 20:57:00 +0100

    MenuItem Service and controller

---
+++ BusinessModule/src/main/java/com/TheCoffeeShop/Services/MenuItemeService.java
@@ -0,0 +1,9 @@
+package com.TheCoffeeShop.Services;
+
+import com.TheCoffeeShop.DTO.MenuItemDTO;
+
+public interface MenuItemeService {
+
+
+  public MenuItemDTO getAllItems();
+}
--- WebModule/src/main/java/com/TheCoffeeShop/Rest/MenuItemControler.java
+++ WebModule/src/main/java/com/TheCoffeeShop/Rest/MenuItemControler.java
@@ -1,4 +1,30 @@
 package com.TheCoffeeShop.Rest;
 
+
+import com.TheCoffeeShop.DTO.MenuItemDTO;
+import com.TheCoffeeShop.Services.MenuItemeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SwaggerDefinition(tags = {@Tag(name="Products Controller" , description = "city controller")})
+@Api(value = "Products Controller" , tags = {"Products Controller"})
 public class MenuItemControler {
+
+
+  private final MenuItemeService  menuItemeService;
+
+  public MenuItemControler(MenuItemeService menuItemeService){
+    this.menuItemeService = menuItemeService;
+  }
+
+
+  @GetMapping("items")
+  public MenuItemDTO getAllItems(){
+    return this.menuItemeService.getAllItems();
+  }
+
 }
